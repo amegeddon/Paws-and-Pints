443ac853e983e81d6ea08afaefbbf3b5062f41ce
amegeddon <ameescook@gmail.com>
2024-03-26 11:36:54 +0000

    Allow usernames with spaces in registration form
    Updated the HTML input pattern attribute to include spaces in the allowed characters for usernames. This change allows users to input usernames containing letters (both uppercase and lowercase), numbers, and spaces. The pattern now validates usernames to be between 3 and 15 characters in length, including spaces.

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
 });
 
+
+/* Code for toggling password visibility, adapted from W3Schools */
 function togglePasswordVisibility() {
     var passwordField = document.getElementById("password");
     if (passwordField.type === "password") {
