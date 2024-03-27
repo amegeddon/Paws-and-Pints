fbf9410d90d0f422ca447ece50d9d09e53821e44
amegeddon <ameescook@gmail.com>
2024-03-27 21:36:28 +0000

    Added interactive star rating system to enhance user experience when providing feedback in review forms. The system dynamically fills stars based on user clicks, allowing for intuitive and visual representation of ratings

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,28 @@ function togglePasswordVisibility() {
         passwordField.type = "password";
     }
 }
+
+/* Interactive star rating system used in write_review.html */
+document.addEventListener('DOMContentLoaded', function() {
+    const ratingIcons = document.querySelectorAll('.star1');
+  
+    ratingIcons.forEach((ratingIcon, index) => {
+      ratingIcon.addEventListener('click', function() {
+        
+        ratingIcons.forEach((star, idx) => {
+          star.classList.remove('filled');
+        });
+  
+        
+        for (let i = 0; i <= index; i++) {
+          ratingIcons[i].classList.add('filled');
+        }
+  
+        // Set the value of the hidden input field to the current rating
+        document.getElementById('user_rating').value = index + 1;
+      });
+    });
+  });
+  
+
+  
